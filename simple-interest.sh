0bacd5fac590ff601e3442625cfc1e06d6ba110b
sangivr <109146548+sangivr@users.noreply.github.com>
2022-07-12 18:38:15 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -5,7 +5,7 @@
    # Do not use this in production. Sample purpose only.
 
    # Author: Upkar Lidder (IBM)
-   # Additional Authors:
+   # Additional Authors:sangvir
    # <your GitHub username>
 
    # Input:
